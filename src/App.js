ca88a97233438e0f684d0b478d170b071c57b214
Robert Leu <robertleu@gmail.com>
2020-03-28 01:20:07 -0700

    main functionality quick and dirty

--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
 import React from 'react';
-import logo from './logo.svg';
+import XLSX from 'xlsx';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    // create ref of instance of input data
+    itemRef = React.createRef();
+    state = {
+        selectedFile: '',
+        selectedToArray: [],
+        results: [],
+        tableHeaders: []
+    };
+
+    // load xlsx for parsing
+    chooseFile = event => {
+        const selectFile = event.target.files[0];
+        this.setState({ selectedFile: selectFile }, function() {
+            console.log(this.state.selectedFile.name);
+        });
+    };
+
+    // parsing data to array of json objects
+    convert = () => {
+        if (this.state.selectedFile) {
+            console.log('hi');
+            var fileReader = new FileReader();
+            fileReader.onload = event => {
+                var data = event.target.result;
+                var workbook = XLSX.read(data, {
+                    type: 'binary',
+                    cellDates: true,
+                    dateNF: 'mm/dd/yyyy;@'
+                });
+                // after data is parsed
+                var first_sheet_name = workbook.SheetNames[0];
+                // get worksheet and parse to json: defval: '' include empty columns
+                var worksheet = workbook.Sheets[first_sheet_name];
+                const selectToArray = XLSX.utils.sheet_to_json(worksheet, { raw: true, defval: '' });
+                this.setState({ selectedToArray: selectToArray }, function() {
+                    console.log(this.state.selectedToArray);
+                    console.log(this.state.selectedToArray[1].prtnum);
+                });
+            };
+            fileReader.readAsBinaryString(this.state.selectedFile);
+        }
+    };
+
+    // create headers for table
+    createHeader = () => {
+        const headers = Object.keys(this.state.selectedToArray[0]);
+        const tableHeaders = headers.map(column => {
+            return {
+                key: column,
+                name: column,
+                editable: true
+            };
+        });
+        this.setState({ tableHeaders }, function() {
+            console.log(this.state.tableHeaders);
+        });
+    };
+
+    // grab items enter and create array and filter inventory to find those items
+    stringToArray = () => {
+        const filterThis = this.state.selectedToArray;
+        const items = this.itemRef.current.value;
+        const filterOptions = items.split(',');
+        // console.log(filterOptions);
+
+        const results = filterThis.filter(function(el) {
+            return filterOptions.indexOf(el.prtnum) >= 0;
+        });
+        console.log(results);
+        this.createHeader();
+    };
+
+    render() {
+        return (
+            <div>
+                <input type="file" name="fileUpload" accept=".xls,.xlsx" onChange={this.chooseFile} />
+                <br />
+                <button type="button" name="uploadExcel" onClick={this.convert}>
+                    Convert
+                </button>
+
+                <input type="text" name="qItems" ref={this.itemRef} placeholder="items..." />
+                <button type="button" onClick={this.stringToArray}>
+                    query
+                </button>
+                <pre id="jsonData"></pre>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/Table.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDataGrid from 'react-data-grid';
+
+class Table extends React.Component {
+    render() {
+        return;
+    }
+}
+
+export default Table;
