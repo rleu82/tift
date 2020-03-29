3fd04355aa5d63f2fb7bd1cbbf350ec889e76f43
Robert Leu <robertleu@gmail.com>
2020-03-29 12:20:34 -0700

    quick css

--- src/App.js
+++ src/App.js
@@ -10,43 +10,43 @@ class App extends React.Component {
         selectedFile: '',
         selectedToArray: [],
         results: '',
-        tableHeaders: []
+        tableHeaders: [],
+        isItDisabled: true
     };
 
     // load xlsx for parsing
     chooseFile = event => {
         const selectFile = event.target.files[0];
-        this.setState({ selectedFile: selectFile }, function() {
+        this.setState({ selectedFile: selectFile, isItDisabled: true }, function() {
             console.log(this.state.selectedFile.name);
+            this.convert();
         });
     };
 
     // parsing data to array of json objects
     convert = () => {
         if (this.state.selectedFile) {
-            console.log('hi');
-            var fileReader = new FileReader();
+            const fileReader = new FileReader();
             fileReader.onload = event => {
-                var data = event.target.result;
-                var workbook = XLSX.read(data, {
+                this.setState({ isItDisabled: true });
+                const data = event.target.result;
+                const workbook = XLSX.read(data, {
                     type: 'binary'
                     // cellDates: true
                 });
                 // after data is parsed
-                var first_sheet_name = workbook.SheetNames[0];
-                // get worksheet and parse to json: defval: '' include empty columns
-                var worksheet = workbook.Sheets[first_sheet_name];
+                const first_sheet_name = workbook.SheetNames[0];
+                // get worksheet and parse to json: defval: '' option includes empty columns
+                const worksheet = workbook.Sheets[first_sheet_name];
                 // const selectToArray = XLSX.utils.sheet_to_json(worksheet, { raw: true, defval: '' });
                 const selectToArray = XLSX.utils.sheet_to_json(worksheet, {
                     // raw: true,
                     dateNF: 'YYYY-MM-DD',
                     defval: ''
                 });
-
-                this.setState({ selectedToArray: selectToArray }, function() {
-                    console.log(this.state.selectedToArray);
-                    console.log(this.state.selectedToArray[1].prtnum);
-                });
+                console.log(selectToArray);
+                // create Table headers after sheet data is generated
+                this.setState({ selectedToArray: selectToArray }, this.createHeader);
             };
             fileReader.readAsBinaryString(this.state.selectedFile);
         }
@@ -62,7 +62,7 @@ class App extends React.Component {
                 accessor: column.toString()
             };
         });
-        this.setState({ tableHeaders }, function() {
+        this.setState({ tableHeaders, isItDisabled: false }, function() {
             console.log(this.state.tableHeaders);
         });
     };
@@ -83,36 +83,68 @@ class App extends React.Component {
     render() {
         if (this.state.results) {
             return (
-                <div>
-                    <input type="file" name="fileUpload" accept=".xls,.xlsx" onChange={this.chooseFile} />
-                    <br />
-                    <button type="button" name="uploadExcel" onClick={this.convert}>
-                        Convert
-                    </button>
-
-                    <input type="text" name="qItems" ref={this.itemRef} placeholder="items..." />
-                    <button type="button" onClick={this.stringToArray}>
-                        query
-                    </button>
-                    <pre id="jsonData"></pre>
-                    <ReactTable data={this.state.results} columns={this.state.tableHeaders} />
-                </div>
+                <React.Fragment>
+                    <header>
+                        <h1>Trans Incredible Filtering Too</h1>
+                    </header>
+                    <div className="container">
+                        <div className="left-wrapper">
+                            <button className="upload-button">Select File</button>
+                            <input type="file" name="file-upload" accept=".xls,.xlsx" onChange={this.chooseFile} />
+                        </div>
+                        <div className="right-wrapper">
+                            <input
+                                type="text"
+                                name="qItems"
+                                ref={this.itemRef}
+                                placeholder="items..."
+                                disabled={this.state.isItDisabled}
+                            />
+                            <button
+                                className="query"
+                                type="button"
+                                onClick={this.stringToArray}
+                                disabled={this.state.isItDisabled}
+                            >
+                                Query
+                            </button>
+                        </div>
+                        <div>
+                            <ReactTable data={this.state.results} columns={this.state.tableHeaders} />
+                        </div>
+                    </div>
+                </React.Fragment>
             );
         }
         return (
-            <div>
-                <input type="file" name="fileUpload" accept=".xls,.xlsx" onChange={this.chooseFile} />
-                <br />
-                <button type="button" name="uploadExcel" onClick={this.convert}>
-                    Convert
-                </button>
-
-                <input type="text" name="qItems" ref={this.itemRef} placeholder="items..." />
-                <button type="button" onClick={this.stringToArray}>
-                    query
-                </button>
-                <pre id="jsonData"></pre>
-            </div>
+            <React.Fragment>
+                <header>
+                    <h1>Trans Incredible Filtering Too</h1>
+                </header>
+                <div className="container">
+                    <div className="left-wrapper">
+                        <button className="upload-button">Select File</button>
+                        <input type="file" name="file-upload" accept=".xls,.xlsx" onChange={this.chooseFile} />
+                    </div>
+                    <div className="right-wrapper">
+                        <input
+                            type="text"
+                            name="qItems"
+                            ref={this.itemRef}
+                            placeholder="items..."
+                            disabled={this.state.isItDisabled}
+                        />
+                        <button
+                            className="query"
+                            type="button"
+                            onClick={this.stringToArray}
+                            disabled={this.state.isItDisabled}
+                        >
+                            Query
+                        </button>
+                    </div>
+                </div>
+            </React.Fragment>
         );
     }
 }
