7567f44c3e339590502cbf891107fa8a628d04a4
Robert Leu <robertleu@gmail.com>
2020-03-28 22:45:34 -0700

    integrate react-table-v6 quick and dirty

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import XLSX from 'xlsx';
-import './App.css';
+import ReactTable from 'react-table-v6';
+import 'react-table-v6/react-table.css';
 
 class App extends React.Component {
     // create ref of instance of input data
@@ -8,7 +9,7 @@ class App extends React.Component {
     state = {
         selectedFile: '',
         selectedToArray: [],
-        results: [],
+        results: '',
         tableHeaders: []
     };
 
@@ -28,15 +29,20 @@ class App extends React.Component {
             fileReader.onload = event => {
                 var data = event.target.result;
                 var workbook = XLSX.read(data, {
-                    type: 'binary',
-                    cellDates: true,
-                    dateNF: 'mm/dd/yyyy;@'
+                    type: 'binary'
+                    // cellDates: true
                 });
                 // after data is parsed
                 var first_sheet_name = workbook.SheetNames[0];
                 // get worksheet and parse to json: defval: '' include empty columns
                 var worksheet = workbook.Sheets[first_sheet_name];
-                const selectToArray = XLSX.utils.sheet_to_json(worksheet, { raw: true, defval: '' });
+                // const selectToArray = XLSX.utils.sheet_to_json(worksheet, { raw: true, defval: '' });
+                const selectToArray = XLSX.utils.sheet_to_json(worksheet, {
+                    // raw: true,
+                    dateNF: 'YYYY-MM-DD',
+                    defval: ''
+                });
+
                 this.setState({ selectedToArray: selectToArray }, function() {
                     console.log(this.state.selectedToArray);
                     console.log(this.state.selectedToArray[1].prtnum);
@@ -51,9 +57,9 @@ class App extends React.Component {
         const headers = Object.keys(this.state.selectedToArray[0]);
         const tableHeaders = headers.map(column => {
             return {
-                key: column,
-                name: column,
-                editable: true
+                id: column,
+                Header: column,
+                accessor: column.toString()
             };
         });
         this.setState({ tableHeaders }, function() {
@@ -67,15 +73,32 @@ class App extends React.Component {
         const items = this.itemRef.current.value;
         const filterOptions = items.split(',');
         // console.log(filterOptions);
-
         const results = filterThis.filter(function(el) {
             return filterOptions.indexOf(el.prtnum) >= 0;
         });
         console.log(results);
-        this.createHeader();
+        this.setState({ results }, this.createHeader);
     };
 
     render() {
+        if (this.state.results) {
+            return (
+                <div>
+                    <input type="file" name="fileUpload" accept=".xls,.xlsx" onChange={this.chooseFile} />
+                    <br />
+                    <button type="button" name="uploadExcel" onClick={this.convert}>
+                        Convert
+                    </button>
+
+                    <input type="text" name="qItems" ref={this.itemRef} placeholder="items..." />
+                    <button type="button" onClick={this.stringToArray}>
+                        query
+                    </button>
+                    <pre id="jsonData"></pre>
+                    <ReactTable data={this.state.results} columns={this.state.tableHeaders} />
+                </div>
+            );
+        }
         return (
             <div>
                 <input type="file" name="fileUpload" accept=".xls,.xlsx" onChange={this.chooseFile} />
